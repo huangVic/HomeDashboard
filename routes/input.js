850d947914a47024f877b0faae3358549a04f349
huangVic <volcomvic@msn.com>
2015-12-03 01:00:07 +0800

    1. add bootstrap lib 2. add jquery.js

--- routes/input.js
+++ routes/input.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('input', { title: '¸ê®ÆºûÅ@' });
 });
 
 module.exports = router;
